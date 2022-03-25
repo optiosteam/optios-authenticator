fed4ed532d439e53c782c1d7fbd8a05821d5a251
Thomas Vanderhaeghen <thomas@optios.net>
2022-03-25 16:26:34 +0100

    Allow proxy settings in http provider

--- src/Provider/HttpClientProvider.php
+++ src/Provider/HttpClientProvider.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 namespace OptiosAuthenticator\Provider;
 
@@ -13,13 +15,20 @@ class HttpClientProvider implements ProviderInterface
     private $authenticationUrl;
 
     /**
+     * @var string|null
+     */
+    private $proxy;
+
+    /**
      * MemoryProvider constructor.
      *
      * @param string $authenticationUrl
+     * @param string|null $proxy
      */
-    public function __construct(string $authenticationUrl)
+    public function __construct(string $authenticationUrl, ?string $proxy = null)
     {
         $this->authenticationUrl = $authenticationUrl;
+        $this->proxy             = $proxy;
     }
 
     /**
@@ -29,12 +38,21 @@ class HttpClientProvider implements ProviderInterface
      */
     public function getToken(string $bearerToken): ?Token
     {
-        $client   = HttpClient::create();
+        $client = HttpClient::create();
+
+        $options = ['json' => ['access_token' => $bearerToken]];
+        if (!empty($this->proxy)) {
+            $options['verify_host'] = false;
+            $options['verify_peer'] = false;
+            $options['proxy']       = $this->proxy;
+        }
+
         $response = $client->request(
             'POST',
             $this->authenticationUrl,
-            ['json' => ['access_token' => $bearerToken]]
+            $options
         );
+
 
         if ($response->getStatusCode() !== 200) {
             return null;
